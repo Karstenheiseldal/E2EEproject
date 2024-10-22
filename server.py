cc6f8d0e601af0d834ca67bcc10af99bffd0b62c
Pozsgai Alex <jatekalex@gmail.com>
2024-10-22 15:13:47 +0200

    Implement shutdown_server

--- server.py
+++ server.py
@@ -4,7 +4,9 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import dh
 
 clients = dict()
-
+server_socket = None
+shutdown_flag = False
+client_sockets = []
 """
 def handle_client(conn, peer_conn):
     #Thread function to relay messages between clients.
@@ -56,26 +58,50 @@ def send_with_length_prefix(conn, data):
 
 def get_connections(client : socket.socket, clients : dict):
     while True:
-        client_message = client.recv(4096).decode()
-        if client_message == "!get_users":
-            response = ""
-            for username, value in clients.items():
-                response += f"Username: {username}, Address: {value['address']} \n"
-            # Send the response to the client
-            client.send(response.encode())
+        try:
+            client_message = client.recv(4096).decode()
+            if client_message == "!get_users":
+                response = ""
+                for username, value in clients.items():
+                    response += f"Username: {username}, Address: {value['address']} \n"
+                # Send the response to the client
+                client.send(response.encode())
+        except :
+            break
+
+def listen_for_shutdown():
+    global server_socket
+    global shutdown_flag
+    while True:
+        command = input("")
+        if command.lower() == 'shutdown':
+            print("Shutting down the server...")
+            shutdown_flag = True
+            if server_socket:
+                server_socket.close()  # Close the server socket
+            for socket in client_sockets:
+                print("Closing client sockets.")
+                socket.close()
+            break
 
 def start_server(host='127.0.0.1', port=5500):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind((host, port))
-        server_socket.listen(2)
-        parameters = dh.generate_parameters(generator=2, key_size=2048)
-        param_bytes = parameters.parameter_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.ParameterFormat.PKCS3
-        )
-        print("Server is listening for connections...")
+    global server_socket
+    global shutdown_flag
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(2)
+    parameters = dh.generate_parameters(generator=2, key_size=2048)
+    param_bytes = parameters.parameter_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.ParameterFormat.PKCS3
+    )
 
-        while True:
+    # Start the shutdown listener thread
+    shutdown_thread = threading.Thread(target=listen_for_shutdown)
+    shutdown_thread.start()
+    print("Server is listening for connections...")
+    while not shutdown_flag:
+        try:
             client, address = server_socket.accept()
             print(f"User connected from {address}")
             print(f"Participants: {len(clients)+1}")
@@ -89,6 +115,12 @@ def start_server(host='127.0.0.1', port=5500):
             }
             handle_client_thread = threading.Thread(target=get_connections, args=(client, clients))
             handle_client_thread.start()
+            client_sockets.append(client)
+        except OSError:
+            print("Server socket has been closed.")
+            break
+
+    shutdown_thread.join()
 
 if __name__ == "__main__":
     start_server()
